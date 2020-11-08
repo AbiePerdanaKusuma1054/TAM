a660c2ff91b98e755cca95af7e8944be01af46c2
AbiePerdanaKusuma1054 <icikiwerblaemblaem@gmail.com>
2020-11-08 18:01:54 +0700

    Action Bar

---
+++ MyActionBar/app/src/main/java/com/example/myactionbar/MenuActivity.java
@@ -0,0 +1,25 @@
+package com.example.myactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
